2eaa15a52377331ac1847acdddf02e3a2d11a722
StevenMartinez94 <albertstevenmf@hotmail.com>
2025-08-04 04:37:44 -0400

    Python source code improved with black
    Also changed allowed hosts on demo.settings.py

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('users', UserViewSet, 'users')
+router.register("users", UserViewSet, "users")
 
 urlpatterns = router.urls
